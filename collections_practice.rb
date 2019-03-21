ca74f37a04a6f89b15af5f50e6e3b3b1be828a77
David Cantoran <davidcantoran36@gmail.com>
2019-03-21 19:18:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(int)
+  int.sort
+end 
 
+def sort_array_desc(arr)
+  arr.sort.reverse
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+  end
+end 
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end 
+  
+def reverse_array(arr) 
+  arr.reverse
+end 
+
+def kesha_maker(arr) 
+  dolla_sign = []
+  arr.each do |word|
+    word[2] = "$"
+    dolla_sign.push(arr)
+  end 
+end 
+  
+def find_a(arr)
+  arr.collect do |str|
+    str.start_with? "a"
+  end 
+  
+end
+  
+  
+  
+  
+  
+  
+  
+  
+  
